442e195fa1ccc9a0bb5ba94e73146559febe340c
gimnakatugampala <gimnakatugampala1@gmail.com>
2023-11-12 10:42:59 +0530

    2023_11_12_Sales_details

--- sales/sales-details.php
+++ sales/sales-details.php
@@ -1,5 +1,6 @@
 ﻿<?php require_once '../includes/header.php' ?>
 <?php require_once '../includes/sidebar.php' ?>
+<?php require_once '../pages/salesdetails.php' ?>
 
 <div class="page-wrapper">
 <div class="content">
@@ -34,6 +35,7 @@
 </li>
 </ul>
 </div>
+
 <div class="invoice-box table-height" style="max-width: 1600px;width:100%;overflow: auto;margin:15px auto;padding: 0;font-size: 14px;line-height: 24px;color: #555;">
 <table cellpadding="0" cellspacing="0" style="width: 100%;line-height: inherit;text-align: left;">
 <tbody><tr class="top">
@@ -42,70 +44,103 @@
 <tbody><tr>
 <td style="padding:5px;vertical-align:top;text-align:left;padding-bottom:20px">
 <font style="vertical-align: inherit;margin-bottom:25px;"><font style="vertical-align: inherit;font-size:14px;color:#7367F0;font-weight:600;line-height: 35px; ">Customer Info</font></font><br>
-<font style="vertical-align: inherit;"><font style="vertical-align: inherit;font-size: 14px;color:#000;font-weight: 400;"> walk-in-customer</font></font><br>
-<font style="vertical-align: inherit;"><font style="vertical-align: inherit;font-size: 14px;color:#000;font-weight: 400;"> <a href="mailto:gimna@gmail.com" class="__cf_email__" >gimna@gmail.com</a></font></font><br>
-<font style="vertical-align: inherit;"><font style="vertical-align: inherit;font-size: 14px;color:#000;font-weight: 400;"> 123456780</font></font><br>
-<font style="vertical-align: inherit;"><font style="vertical-align: inherit;font-size: 14px;color:#000;font-weight: 400;"> N45 , Dhaka</font></font><br>
+<font style="vertical-align: inherit;"><font style="vertical-align: inherit;font-size: 14px;color:#000;font-weight: 400;"><?php echo $salesorders[0]["cusname"];?></font></font><br>
+<font style="vertical-align: inherit;"><font style="vertical-align: inherit;font-size: 14px;color:#000;font-weight: 400;"> <a href="<?php echo $salesorders[0]["cusemail"];?>" class="__cf_email__" ><?php echo $salesorders[0]["cusemail"];?></a></font></font><br>
+<font style="vertical-align: inherit;"><font style="vertical-align: inherit;font-size: 14px;color:#000;font-weight: 400;"> <?php echo "+94".$salesorders[0]["cusphone"];?></font></font><br>
+<font style="vertical-align: inherit;"><font style="vertical-align: inherit;font-size: 14px;color:#000;font-weight: 400;"> <?php echo $salesorders[0]["cusaddress"];?></font></font><br>
 </td>
 
 <td style="padding:5px;vertical-align:top;text-align:left;padding-bottom:20px">
 <font style="vertical-align: inherit;margin-bottom:25px;"><font style="vertical-align: inherit;font-size:14px;color:#7367F0;font-weight:600;line-height: 35px; ">Invoice Info</font></font><br>
-<font style="vertical-align: inherit;"><font style="vertical-align: inherit;font-size: 14px;color:#000;font-weight: 400;"> Reference </font></font><br>
+<font style="vertical-align: inherit;"><font style="vertical-align: inherit;font-size: 14px;color:#000;font-weight: 400;"> Sales Code </font></font><br>
 <font style="vertical-align: inherit;"><font style="vertical-align: inherit;font-size: 14px;color:#000;font-weight: 400;"> Payment Status</font></font><br>
  <font style="vertical-align: inherit;"><font style="vertical-align: inherit;font-size: 14px;color:#000;font-weight: 400;"> Status</font></font><br>
 </td>
 <td style="padding:5px;vertical-align:top;text-align:right;padding-bottom:20px">
 <font style="vertical-align: inherit;margin-bottom:25px;"><font style="vertical-align: inherit;font-size:14px;color:#7367F0;font-weight:600;line-height: 35px; ">&nbsp;</font></font><br>
-<font style="vertical-align: inherit;"><font style="vertical-align: inherit;font-size: 14px;color:#000;font-weight: 400;">SL0101 </font></font><br>
-<font style="vertical-align: inherit;"><font style="vertical-align: inherit;font-size: 14px;color:#2E7D32;font-weight: 400;"> Paid</font></font><br>
-<font style="vertical-align: inherit;"><font style="vertical-align: inherit;font-size: 14px;color:#2E7D32;font-weight: 400;"> Completed</font></font><br>
+<font style="vertical-align: inherit;"><font style="vertical-align: inherit;font-size: 14px;color:#000;font-weight: 400;"><?php echo $salesorders[0]["socode"];?></font></font><br>
+
+<font style="vertical-align: inherit;">
+<?php if ($salesorders[0]["paidstatusid"] == "1"): ?>
+<font class="text-danger">Not Paid</font></font><br>
+<?php elseif ($salesorders[0]["paidstatusid"] == "2"): ?>
+<font class="text-warning"> Advance</font></font><br>
+<?php elseif ($salesorders[0]["paidstatusid"] == "3"): ?>
+<font class="text-success"> Paid</font></font><br>
+<?php elseif ($salesorders[0]["paidstatusid"] == "4"): ?>
+<font class="text-info"> Draft</font></font><br>
+<?php endif; ?>
+
+<font style="vertical-align: inherit;">
+<?php if ($salesorders[0]["sid"] == "1"): ?>
+<font class="text-success"> Completed</font></font><br>
+<?php elseif ($salesorders[0]["sid"] == "2"): ?>
+<font class="text-primary"> Inprogress</font></font><br>
+<?php elseif ($salesorders[0]["sid"] == "3"): ?>
+<font class="text-danger"> Canceled</font></font><br>
+<?php elseif ($salesorders[0]["sid"] == "4"): ?>
+<font class="text-info"> Quotation</font></font><br>
+<?php endif; ?>
+
 </td>
 </tr>
 </tbody>
 </table>
 </td>
 </tr>
+
+<!-- Table -->
 <tr class="heading " style="background: #F3F2F7;">
 <td style="padding: 5px;vertical-align: middle;font-weight: 600;color: #5E5873;font-size: 14px;padding: 10px; ">
 Product Name
-</td>
-<td style="padding: 5px;vertical-align: middle;font-weight: 600;color: #5E5873;font-size: 14px;padding: 10px; ">
-Price
 </td>
 <td style="padding: 5px;vertical-align: middle;font-weight: 600;color: #5E5873;font-size: 14px;padding: 10px; ">
 QTY
 </td>
 
 <td style="padding: 5px;vertical-align: middle;font-weight: 600;color: #5E5873;font-size: 14px;padding: 10px; ">
-Dicount
+Price (Rs. )
+</td>
+
+
+<td style="padding: 5px;vertical-align: middle;font-weight: 600;color: #5E5873;font-size: 14px;padding: 10px; ">
+Dicount (Rs.)
 </td>
 
 <td style="padding: 5px;vertical-align: middle;font-weight: 600;color: #5E5873;font-size: 14px;padding: 10px; ">
-Subtotal
+Subtotal (Rs.)
 </td>
 </tr>
+
+<?php foreach ($productlist as $item): ?>
+
 <tr class="details" style="border-bottom:1px solid #E9ECEF ;">
 <td style="padding: 10px;vertical-align: top; display: flex;align-items: center;">
 <img src="../assets/img/product/noimage.png" alt="img" class="me-2" style="width:40px;height:40px;">
-Macbook pro
-</td>
-<td style="padding: 10px;vertical-align: top; ">
-1500.00
+<?php echo $item["productname"]; ?>
 </td>
 
 <td style="padding: 10px;vertical-align: top; ">
-1.00
+<?php echo $item["QTY"]; ?>
 </td>
 
 <td style="padding: 10px;vertical-align: top; ">
-00.00
+<?php echo $item["price"]; ?>
+</td>
+
+
+
+<td style="padding: 10px;vertical-align: top; ">
+<?php echo $item["discount"]; ?>
 </td>
 <td style="padding: 10px;vertical-align: top; ">
-1500.00
+<?php echo floatval($item["QTY"]) * floatval($item["price"]) - floatval($item["discount"]); ?>
 </td>
 </tr>
 
-<tr class="details" style="border-bottom:1px solid #E9ECEF ;">
+<?php endforeach;?>
+
+<!-- <tr class="details" style="border-bottom:1px solid #E9ECEF ;">
 <td style="padding: 10px;vertical-align: top; display: flex;align-items: center;">
 <img src="../assets/img/product/noimage.png" alt="img" class="me-2" style="width:40px;height:40px;">
 Apple Earpods
@@ -123,9 +158,9 @@ Apple Earpods
 <td style="padding: 10px;vertical-align: top; ">
 1500.00
 </td>
-</tr>
+</tr> -->
 
-<tr class="details" style="border-bottom:1px solid #E9ECEF ;">
+<!-- <tr class="details" style="border-bottom:1px solid #E9ECEF ;">
 <td style="padding: 10px;vertical-align: top; display: flex;align-items: center;">
 <img src="../assets/img/product/noimage.png" alt="img" class="me-2" style="width:40px;height:40px;">
 samsung
@@ -144,9 +179,11 @@ samsung
 <td style="padding: 10px;vertical-align: top; ">
 1500.00
 </td>
-</tr>
+</tr> -->
 
-</tbody></table>
+</tbody>
+</table>
+
 </div>
 <div class="row">
 
@@ -159,20 +196,21 @@ samsung
 <div class="total-order w-100 max-widthauto m-auto mb-4">
 <ul>
 <li>
-<h4>Grand Total</h4>
-<h5>Rs. 0.00</h5>
+<h4>Total Discount</h4>
+<h5>Rs. <?php echo $salesorders[0]["DIS"]; ?></h5>
 </li>
 <li>
-<h4>Discount</h4>
-<h5>Rs. 0.00</h5>
+<h4>Grand Total</h4>
+<h5>Rs. <?php echo $salesorders[0]["grandtotal"]; ?></h5>
 </li>
+
 <li>
 <h4>Paid Amount</h4>
-<h5>Rs. 0.00</h5>
+<h5>Rs. <?php echo $salesorders[0]["paidamount"]; ?></h5>
 </li>
 <li class="total">
 <h4>To Be Paid</h4>
-<h5>Rs. 0.00</h5>
+<h5>Rs. <?php echo floatval($salesorders[0]["grandtotal"]) - floatval($salesorders[0]["paidamount"]); ?></h5>
 </li>
 </ul>
 </div>
