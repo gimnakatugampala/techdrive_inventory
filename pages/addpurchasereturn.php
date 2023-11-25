6a38fd1599ab43147613161413a43e541cf6ba6a
gimnakatugampala <gimnakatugampala1@gmail.com>
2023-11-26 01:19:43 +0530

    2023_11_26_add_por_stock

--- pages/addpurchasereturn.php
+++ pages/addpurchasereturn.php
@@ -29,13 +29,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $invoiceissueddate = date('Y-m-d H:i:s');
     $createddate = date('Y-m-d H:i:s');
 
-    // $min = 1;
-    // $max = 10000000000;
-    // $pocode = rand($min, $max);
-
-    // $min = 1;
-    // $max = 10000000000;
-    // $picode = rand($min, $max);
 
     $insertPurchaseOrderSQL = "INSERT INTO tbpurchaseorderreturn (porcode, supid, sid ,created_date,description) VALUES 
     ('$pocode', '$selectSup', '$progressstatus','$createddate','$desc')";
@@ -47,6 +40,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $quantity = $row['quantity'];
             $price = $row['price'];
             $discount = $row['discount'];
+
+            if($progressstatus == "1"){
+
+                // Update the New Product Quantity
+                $query = "SELECT quantity FROM tbproduct WHERE id = $product";
+                $result = $conn->query($query);
+    
+                if ($result->num_rows > 0) {
+                    $current_quantity = $result->fetch_assoc()['quantity'];
+                    $new_quantity = $current_quantity - $quantity;
+    
+                    // Update the database with the new quantity
+                    $update_query = "UPDATE tbproduct SET quantity = $new_quantity WHERE id = $product";
+    
+                    if ($conn->query($update_query) === TRUE) {
+                        // echo "sucess";
+                    } else {
+                        echo "Error updating quantity: " . $conn->error;
+                    }
+                }
+                // Update the New Product Quantity
+                }
 
             $sql = "INSERT INTO tbpurchaseorderreturnitem (product_id, porid, qty, price, discount) VALUES 
             ('$product', '$insertedPurchaseOrderID', '$quantity', '$price', '$discount')";
