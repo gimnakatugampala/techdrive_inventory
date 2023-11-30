567c6dd5c5b9bfcf77f9db3fe2629edf752b1083
gimnakatugampala <gimnakatugampala1@gmail.com>
2023-11-30 21:01:13 +0530

    2023_11_30_sales_return

--- assets/js/editsalesreturn.js
+++ assets/js/editsalesreturn.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    console.log("13")
+    
     var dropdown = document.getElementById("productcmb");
     const tableBody = $("#bodyESORL");
   
@@ -10,6 +10,7 @@ $(document).ready(function () {
        let loadData;
   
       var items = [];
+      var pro_qty = [];
   
      // Get The Code From URL
      const urlParams = new URLSearchParams(window.location.search);
@@ -55,6 +56,7 @@ $(document).ready(function () {
   
             }else{
               populateTable(data);
+              pro_qty.push(data[0])
             }
 
         },
@@ -73,7 +75,7 @@ $(document).ready(function () {
           );
           row.append(
             "<td><input type='number' class='form-control price' value=" +
-              plist.buyingprice +
+              plist.sellingprice +
               " name='pprice'></td>"
           );
           row.append(
@@ -233,6 +235,123 @@ $(document).ready(function () {
         console.log(loadData.Productlists)
         console.log(data)
         console.log(dis)
+
+        const combinedArray = [...loadData.Productlists, ...pro_qty];
+
+
+         // ---------------- PRODUCT ITEM ADDED VALIDATION -----------------
+
+
+        // Check if the Quantity is Sufficient
+      for(let i = 0; i < data.length; i++){
+        if(parseInt(data[i].quantity) > parseInt(combinedArray[i].quantity)){
+          Swal.fire({
+            icon: "error",
+            title: "Quantity Error",
+            text: `${combinedArray[i].productname} has a Max QTY of ${combinedArray[i].quantity}`,
+          });
+
+          return
+        }
+      }
+
+        // Check if the QTY is zero
+        for(let i = 0; i < data.length; i++){
+          if(parseInt(data[i].quantity) == 0){
+            Swal.fire({
+              icon: "error",
+              title: "Quantity Error",
+              text: `${combinedArray[i].productname} QTY Cannot be Zero`,
+            });
+  
+            return
+          }
+        }
+
+        // Check if the QTY is Negative
+        for(let i = 0; i < data.length; i++){
+          if(parseInt(data[i].quantity) < 0){
+            Swal.fire({
+              icon: "error",
+              title: "Quantity Error",
+              text: `${combinedArray[i].productname} QTY Cannot be Negative`,
+            });
+  
+            return
+          }
+        }
+
+        // ----- PRODUCT PRICE ------
+      // Check if the Price is Sufficient
+      for(let i = 0; i < data.length; i++){
+        if(parseInt(data[i].price) > parseInt(combinedArray[i].sellingprice)){
+          Swal.fire({
+            icon: "error",
+            title: "Price Error",
+            text: `${combinedArray[i].productname} has a Max Price of Rs.${combinedArray[i].sellingprice}`,
+          });
+
+          return
+        }
+      }
+
+        // Check if the Price is zero
+        for(let i = 0; i < data.length; i++){
+          if(parseInt(data[i].price) == 0){
+            Swal.fire({
+              icon: "error",
+              title: "Price Error",
+              text: `${combinedArray[i].productname} Price Cannot be Zero`,
+            });
+  
+            return
+          }
+        }
+
+        // Check if the Price is Negative
+        for(let i = 0; i < data.length; i++){
+        if(parseInt(data[i].price) < 0){
+          Swal.fire({
+            icon: "error",
+            title: "Price Error",
+            text: `${combinedArray[i].productname} Price Cannot be Negative`,
+          });
+
+          return
+        }
+      }
+
+         // ----- DISCOUNT PRICE ------
+  
+          // Check if the Discount is Negative
+          for(let i = 0; i < data.length; i++){
+            if(parseInt(data[i].discount) < 0){
+              Swal.fire({
+                icon: "error",
+                title: "Price Error",
+                text: `${combinedArray[i].productname} Discount Price Cannot be Negative`,
+              });
+  
+              return
+            }
+          }
+
+          // Discount cannot be greater than the Price
+        for(let i = 0; i < data.length; i++){
+          if(parseInt(data[i].discount) > parseInt(data[i].quantity) * parseInt(data[i].price)){
+            Swal.fire({
+              icon: "error",
+              title: "Price Error",
+              text: `Discount Price Cannot be Greater than ${combinedArray[i].productname} Price`,
+            });
+
+            return
+          }
+        }
+
+
+
+      // ---------------- PRODUCT ITEM ADDED VALIDATION -----------------
   
         if(grandTotal == 0){
           console.log("old grand total "+oldgrandTotal)
